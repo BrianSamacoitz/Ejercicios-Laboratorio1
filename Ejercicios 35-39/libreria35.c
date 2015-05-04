c1842f1c81edb08b57158c24d42cd2a5109767c7
BrianSamacoitz <brian.samacoitz@hotmail.com>
2015-05-04 14:54:28 -0300

    Revert "Revert "Revert "ejercicios"""
    This reverts commit a11b45a661ad0e7866118e7ace0bd123d2e96ec5.

--- Ejercicios 35-39/libreria35.c
+++
@@ -1,9 +0,0 @@
-int control(int num)
-{
-          if(num<-100||num>100)
-          {
-           printf("Error, vuelva a ingresar el numero dentro del rango\n");
-           scanf("%d",&num);                     
-          }
-return num;
-}
